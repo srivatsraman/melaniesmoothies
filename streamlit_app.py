9d1963f4a4e7ba5da91effcba83adf99438d7243
srivatsraman <41300177+srivatsraman@users.noreply.github.com>
2025-04-05 16:34:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,8 @@ if ingredients_list:
       st.write (ingredients_string)
       my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-      time_to_insert = st.button('Submit Order')
-      if time_to_insert:
-         session.sql(my_insert_stmt).collect()
-         st.success('Your Smoothie is ordered ',icon="✅")
-         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+   time_to_insert = st.button('Submit Order')
+   if time_to_insert:
+      session.sql(my_insert_stmt).collect()
+      st.success('Your Smoothie is ordered ',icon="✅")
+      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
