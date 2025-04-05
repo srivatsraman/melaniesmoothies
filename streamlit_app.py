b83b9ae530d83822f4f6fc9e055755af47b26293
srivatsraman <41300177+srivatsraman@users.noreply.github.com>
2025-04-05 16:32:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,4 @@ if ingredients_list:
       if time_to_insert:
          session.sql(my_insert_stmt).collect()
          st.success('Your Smoothie is ordered ',icon="✅")
-
-
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
